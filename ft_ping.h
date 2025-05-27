f701ed05ec21e8f3848eb012106feebe4bf803cf
Fabio Carvalho Lopes <facarval@f1br3s8.clusters.42paris.fr>
2025-05-27 17:56:27 +0200

    Ping works partially

--- ft_ping.h
+++ ft_ping.h
@@ -7,8 +7,14 @@
 #include <features.h>
 #include <signal.h>
 #include <strings.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <netdb.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h> 
 
 void opt_man(void);
 int ft_ping(char *address);
